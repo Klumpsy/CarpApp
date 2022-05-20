0679b6ce20edea1eb51f76ec011b6630ca720c85
bart <bart_klumperman@Live.nl>
2022-05-20 16:36:21 +0200

    tests for badges done

--- tests/Entity/waterTest.php
+++ tests/Entity/waterTest.php
@@ -5,13 +5,44 @@ use App\Entity\Water;
 
 class WaterTest extends TestCase
 {
+    private $water;
+
+    protected function setUp():void
+    {
+        $this->water = new Water();
+    }
+
     public function test_water_name_can_be_set()
     {
-        $water = new Water();
-        $water->setName('Kanaal');
-        $waterName = $water->getName();
+
+        $this->water->setName('Kanaal');
+        $waterName = $this->water->getName();
 
         $this->assertSame('Kanaal', $waterName);
+    }
+
+    public function test_water_saves_BigFish_badge()
+    {
+        $this->water->setBigFish(true);
+        $this->assertSame(true, $this->water->isBigFish());
+    }
+
+    public function test_water_saves_crayfish_badge()
+    {
+        $this->water->setKreeften(true);
+        $this->assertSame(true, $this->water->isKreeften());
+    }
+
+    public function test_water_saves_small_fish_badge()
+    {
+        $this->water->setSmallFish(true);
+        $this->assertSame(true, $this->water->isSmallFish());
+    }
+
+    public function test_water_saves_hard_water()
+    {
+        $this->water->setMoeilijk(true);
+        $this->assertSame(true, $this->water->isMoeilijk());
     }
 
 }
